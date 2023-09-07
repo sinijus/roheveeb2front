b39a23b236249280053f56702d85979f20c0db75
Anna-Maria <anna.rannamagi@gmail.com>
2023-09-07 14:44:16 +0300

    F1 WIP

---
+++ src/views/LoginRegisterBox.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="col justify-content-center" @keydown.enter="login">
+   <div class="col-4" >
+     <input type="text" class="form-control" placeholder="e-mail" aria-describedby="addon-wrapping">
+   </div>
+   <div class="col-4">
+     <input type="text" class="form-control" placeholder="parool" aria-describedby="addon-wrapping">
+   </div>
+ </div>
+</template>
+<script>
+export default {
+  name: 'LoginRegisterBox'
+}
+</script>
