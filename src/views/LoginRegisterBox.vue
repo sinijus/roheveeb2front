0412b78034a2157cb642daa5c9fde47f15ab08c5
simmosaar <simmo@valiant.ee>
2023-09-07 15:54:00 +0300

    F1 with LOGINBOX

--- src/views/LoginRegisterBox.vue
+++ src/views/LoginRegisterBox.vue
@@ -1,10 +1,18 @@
 <template>
- <div class="col justify-content-center" @keydown.enter="login">
+ <div class="col"  @keydown.enter="login">
    <div class="col-4" >
      <input type="text" class="form-control" placeholder="e-mail" aria-describedby="addon-wrapping">
    </div>
    <div class="col-4">
      <input type="text" class="form-control" placeholder="parool" aria-describedby="addon-wrapping">
+     <br>
+
+   </div>
+
+   <div>
+     <div><button type="button" class="btn btn-link">Registreeri</button></div>
+     <br>
+     <div> <button type="button" class="btn btn-success">Logi sisse</button> </div>
    </div>
  </div>
 </template>
