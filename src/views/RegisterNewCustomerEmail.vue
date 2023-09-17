f84f17f89968691869e08887b39f05532d00a901
simmosaar <simmo@valiant.ee>
2023-09-17 15:45:56 +0300

    F7 almost done - "tagasi" button does not work, "maakond" and "indeks" are not aligned well.

---
+++ src/views/RegisterNewCustomerEmail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <form class="row justify-content-center">
+      <div class="col col-3 mt-3">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">E-maili aadress</label>
+          <input type="email" class="form-control" id="insertCustomeremail" aria-describedby="insertCustomeremail" placeholder="Sisesta oma e-mail*">
+          <div id="insertCustomeremail"></div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'registerNewCustomerEmail'
+}
+</script>
