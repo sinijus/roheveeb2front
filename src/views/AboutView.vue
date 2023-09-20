d781f0b2890753061d2f6966c2817f4c43e54878
simmosaar <simmo@valiant.ee>
2023-09-20 11:16:07 +0300

    About tehtud.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <font-awesome-icon :icon="['fas', 'user-secret']" />
+    <h1>Meist</h1>
+    <font-awesome-icon :icon="['fas', 'user-secret']"/>
+
+    <h3 class="has-text-align-center">OÜ Päikesetoit<br>registrikood 80626251<br>info@roheveeb.ee<br>pangaarve
+      EE112180221082120538<br>SWEDBANK (SWIFT kood/BIC HABAEE2X)</h3>
+  </div>
+
+  <div>
+    <h1 itemprop="headline">Koostööpartnerid</h1>
+    &nbsp; &nbsp;
+    <div>
+     <img
+          src="https://scontent.ftll3-1.fna.fbcdn.net/v/t39.30808-6/307928454_454890770008542_3525371015259855933_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=2mM1ITQuM2YAX_wiRtv&_nc_ht=scontent.ftll3-1.fna&oh=00_AfB7gGhaUWE_ruCYY_4mYskfwAxofJDD470b49LKMaH5WQ&oe=650EFC60"
+          alt="" width="180" height="180">&nbsp;&nbsp;<img
+          src="https://scontent.ftll3-2.fna.fbcdn.net/v/t39.30808-6/300497246_464099335724936_6233719525318977023_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=81_dDXMtl90AX806-Kg&_nc_ht=scontent.ftll3-2.fna&oh=00_AfA8AEVWbkpbPxUh3ezWhmW2JgeNk35j-gHQnnCpyEnznQ&oe=650FBA5E"
+          alt="" width="180" height="180">&nbsp;&nbsp;<img
+          src="https://scontent.ftll3-2.fna.fbcdn.net/v/t39.30808-6/293727210_548232270293956_6281408803990496519_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=JsMtaAQA2qsAX9DTu4M&_nc_ht=scontent.ftll3-2.fna&oh=00_AfAcHsvbiyATIhhtOhKqXv5cthV1kd6SAv2gkfe6tYspJA&oe=65100BB5"
+          alt="" width="180" height="180">&nbsp;&nbsp; <img
+          src="https://scontent.ftll3-2.fna.fbcdn.net/v/t39.30808-6/327380527_1225783634709318_2850796215297196732_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=I32lamphkdQAX_Ynuyn&_nc_ht=scontent.ftll3-2.fna&oh=00_AfDoDCUiUuJfzX_x6fzYjrj3JHH2dZUM49dIHGV_y-SS8w&oe=65105F92"
+          alt="" width="180" height="180">&nbsp;&nbsp; <img
+        src="https://mulgimaa.ee/userfiles/image/Mulgi-vaiketootjate-liit-logo-512x512-1.svg"
+        alt="" width="180" height="180">&nbsp;&nbsp;
+    </div>
   </div>
 </template>
 
+<script setup>
+</script>
