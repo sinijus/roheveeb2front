4549e1fed93fff42f353bdf25709604c7a031275
simmosaar <simmo@valiant.ee>
2023-09-20 16:04:47 +0300

    Esilehele lisatud logo.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,17 @@
   <div class="page">
     <div class="row">
       <div class="col justify-content-right">
-        <div class="product-item"> {{ introductionText }}</div>
+        <div class="introduction-text"> {{ introductionText }}</div>
       </div>
       <div class="col">
         <LoginRegisterBox @event-udpate-nav-menu="handleNavMenuUpdate()"/>
       </div>
     </div>
-  </div>
+    <div class="adjustable-image">
+      <img src="https://media.voog.com/0000/0048/2898/photos/porgand.png">
+    </div>
+    </div>
+
 </template>
 
 <script>
@@ -16,10 +20,14 @@
 import {HOME_VIEW_INTRODUCTION} from "@/assets/text/text.storage";
 import LoginRegisterBox from "@/components/LoginRegisterBox.vue";
 import ModalRegister from "@/components/modal/ModalRegister.vue";
+import ImageInput from "@/views/ImageInput.vue";
+import ContactTextBox from "@/views/ContactTextBox.vue";
 
 export default {
   name: 'HomeView',
   components: {
+    ContactTextBox,
+    ImageInput,
     ModalRegister,
     LoginRegisterBox
   },
