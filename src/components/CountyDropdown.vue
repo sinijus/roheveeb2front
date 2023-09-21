abe617b8d54e18f6a732a835745f7fd00d2deebb
Anna-Maria <anna.rannamagi@gmail.com>
2023-09-21 16:02:56 +0300

    fix

--- src/components/CountyDropdown.vue
+++ src/components/CountyDropdown.vue
@@ -1,56 +1,53 @@
 <template>
-  <template>
-    <select v-model="selectedCiyId" @change="emitSelectedCityId" class="form-select"
-            aria-label="Default select example">
-      <option selected :value="0">Kõik linnad</option>
-      <option v-for="city in cities" :value="city.cityId" :key="city.cityId">{{ city.cityName }}</option>
-    </select>
-  </template>
-  <script>
-    import router from "@/router";
-
-    export default {
-      name: 'CitiesDropdown',
-      data() {
-        return {
-          selectedCityId: 0,
-
-          cities: [
-            {
-              cityId: 0,
-              cityName: ''
-            }
-          ]
-        }
-      },
-      methods: {
-
-        getCities() {
-          this.$http.get("/cities")
-              .then(response => {
-                this.cities = response.data
-              })
-              .catch(error => {
-                router.push({name: 'errorRoute'})
-              })
-        },
-        emitSelectedCityId() {
-          this.$emit('event-update-selected-city-id', this.selectedCityId)
-        },
-
-      },
-      beforeMount() {
-        this.getCities()
-      }
-
-    }
-  </script>
+  <select v-model="selectedCountyId" @change="emitSelectedCountyId" class="form-select"
+          aria-label="Default select example">
+    <option selected :value="0">Kõik maakonnad</option>
+    <option v-for="county in counties" :value="county.countyId" :key="county.countyId">{{
+        county.countyName
+      }}
+    </option>
+  </select>
 </template>
 <script>
+import router from "@/router";
+
 export default {
-  name: 'countyDropdown'
+  name: 'CountyDropdown',
+  data() {
+    return {
+      selectedCountyId: 0,
+
+      counties: [
+        {
+          countyId: 0,
+          countyName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+
+    getCounties() {
+      this.$http.get("/counties")
+          .then(response => {
+            this.counties = response.data
+          })
+          .catch(error => {
+            router.push({name: 'errorRoute'})
+          })
+    },
+    emitSelectedCountyId() {
+      this.$emit('event-update-selected-county-id', this.selectedCountyId)
+    },
+
+  },
+  beforeMount() {
+    this.getCounties()
+  }
+
 }
 </script>
+
 <style scoped>
 
 fieldset {
--- src/views/ProfileCustomerView.vue
+++ src/views/ProfileCustomerView.vue
@@ -1,46 +1,78 @@
 <template>
   <div>
-    <div>
-      <label for="Eesnimi">Eesnimi:</label>
-<!--      <input type="text" id="nimi" v-model="customerProfileInfo.firstName"/>-->
-      {{customerProfileInfo.firstName}}
+
+    <div class="container text-center">
+
+      <div class="row justify-content-center">
+        <div class="col col-4">
+          <div class="row mb-5">
+            <h1>Kliendi profiil</h1>
+          </div>
+
+          <div class="row">
+            <div  class="input-group mb-3">
+              <span class="input-group-text">Eesnimi</span>
+              <input v-model="customerProfileInfo.firstName" type="text" class="form-control">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="input-group mb-3">
+              <span class="input-group-text">Perenimi</span>
+              <input v-model="customerProfileInfo.lastName" type="text" class="form-control">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="input-group mb-3">
+              <span class="input-group-text">Aadress</span>
+              <input v-model="customerProfileInfo.userLocationAddress" type="text" class="form-control">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="input-group mb-3">
+              <CountyDropdown ref="countyDropdownRef"/>
+            </div>
+          </div>
+
+
+
+
+
+
+          <div class="row">
+            <div class="input-group mb-3">
+              <span class="input-group-text">Telefoninumber</span>
+              <input v-model="customerProfileInfo.phoneNumber" type="number" class="form-control">
+            </div>
+          </div>
+
+
+
+          <div class="row">
+            <div class="input-group mb-3">
+              <span class="input-group-text">Postiindeks</span>
+              <input v-model="customerProfileInfo.userLocationPostalCode" type="number" class="form-control">
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
+
     <div>
-      <label for="Perenimi">Perenimi:</label>
-<!--      <input type="text" id="nimi" v-model="formData.lastName"/>-->
-      {{customerProfileInfo.lastName}}
+      <a href="/shop" class="btn btn-outline-success m-2" >Tagasi poe vaatesse</a>
+      <button type="button" class="btn btn-success" @click="updateCustomerProfile" >Salvesta muudatused</button>
     </div>
-    <div>
-      <label for="email">e-mail:</label>
-<!--      <input type="text" id="email" v-model="formData.email"/>-->
-      {{customerProfileInfo.userEmail}}
-    </div>
-    <div>
-      <label for="aadress">Aadress:</label>
-<!--      <input type="text" id="aadress" v-model="formData.aadress"/>-->
-      {{customerProfileInfo.userLocationAddress}}
-    </div>
-    <div>
-      <label for="maakond">Maakond:</label>
-<!--      <input type="text" id="maakond" v-model="formData.maakond"/>-->
-      {{customerProfileInfo.userLocationCountyName}}
-    </div>
-    <div>
-      <label for="postiindeks">Postiindeks:</label>
-<!--      <input type="text" id="postiindeks" v-model="formData.postiindeks"/>-->
-      {{customerProfileInfo.userLocationPostalCode}}
-    </div>
-    <div>
-      <label for="telefon">Telefon:</label>
-<!--      <input type="text" id="telefon" v-model="formData.telefon"/>-->
-      {{customerProfileInfo.phoneNumber}}
-    </div>
-    <button @click="saveToDatabase">Save</button>
   </div>
 </template>
 
 <script>
+import CountyDropdown from "@/components/CountyDropdown.vue";
+
 export default {
+  components: {CountyDropdown},
   data() {
     return {
       customerProfileInfo: {
@@ -71,12 +103,17 @@ export default {
       ).then(response => {
         // Siit saame kätte JSONi  ↓↓↓↓↓↓↓↓
         this.customerProfileInfo = response.data
+        this.$refs.countyDropdownRef.selectedCountyId = this.customerProfileInfo.userLocationCountyId
         // alert('getCustomerProfile then')
       }).catch(error => {
         // Siit saame kätte errori JSONi  ↓↓↓↓↓↓↓↓
         this.errorResponseCustomer = error.response.data
         alert('error')
       })
+    },
+
+    updateCustomerProfile() {
+      alert("Teeme tulevikus - skoop 2 :)")
     },
 
   },
